d50d2bd6267ba5d433156de84e0f6db39f91c10a
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-06-03 17:38:08 +0700

    membuat function setXY dan display sebagai public agar dapat terlihat/di akses umum

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -4,5 +4,17 @@ using namespace std;
 class abstraksiklas {
 private: string x, y;
 
+public:
 
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
 };

db27a551d93b7c5e0f3ea185ab8b20a5e135ff09
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-06-03 18:24:55 +0700

    membuat class pekerja sebagai keturunan dari orang dengan function untuk umur pekerja

--- Multiwaris.cpp
+++ Multiwaris.cpp
@@ -9,3 +9,11 @@ public:
             cout << "orang dibuat dengan umur " << umur << "\n" << endl;
         }
 };
+
+class pekerja : virtual public orang {
+public: 
+    pekerja(int pUmur) :
+        orang(pUmur) {
+            cout << "pekerja dibuat\n" << endl;
+         }
+};
